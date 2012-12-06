7fab677324b13e254e3938c8fc70c8da181c1066
Stephen Kelly <stephen.kelly@kdab.com>
2012-12-06 14:38:00 +0100

    Add unit tests for the cmake files.
    Change-Id: I2e07862e0e13d8a9747691d219185edbe6c86274
    Reviewed-by: David Faure (KDE) <faure@kde.org>

--- tests/auto/auto.pro
+++ tests/auto/auto.pro
@@ -1,3 +1,4 @@
 TEMPLATE=subdirs
 SUBDIRS=\
-           qx11info
+           qx11info \
+           cmake
---
+++ tests/auto/cmake/test_qx11_module/main.cpp
@@ -0,0 +1,12 @@
+
+#include <QX11Info>
+#include <QtX11Support/QX11Info>
+#include <QtX11Support/QtX11Support>
+#include <QtX11Support>
+
+int main(int argc, char **argv)
+{
+  QX11Info::appScreen();
+
+  return 0;
+}
