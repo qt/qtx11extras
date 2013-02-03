ed6b192b023903fa3e879adf4c0f6912bb361040
David Faure <faure@kde.org>
2013-02-03 12:15:55 +0100

    Remove QT_BEGIN_HEADER/QT_END_HEADER macro usage
    These were just removed in qtbase dev.
    
    Change-Id: Id7683e7407cc4615ca3ed4bc2d262fa21bc3ba48
    Reviewed-by: Laszlo Papp <lpapp@kde.org>
    Reviewed-by: Sergio Ahumada <sergio.ahumada@digia.com>

--- src/x11extras/qx11info_x11.h
+++ src/x11extras/qx11info_x11.h
@@ -48,8 +48,6 @@
 typedef struct _XDisplay Display;
 struct xcb_connection_t;
 
-QT_BEGIN_HEADER
-
 QT_BEGIN_NAMESPACE
 
 class Q_X11EXTRAS_EXPORT QX11Info
@@ -75,8 +73,6 @@ private:
 };
 
 QT_END_NAMESPACE
-
-QT_END_HEADER
 
 #endif // QX11INFO_X11_H
 
